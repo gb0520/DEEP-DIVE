9c1ce11ac1a3cc8b9cbd00111329842a3b9bba4a
psk <77984774+youchanje30@users.noreply.github.com>
2023-12-30 05:18:14 +0900

    게임 매니저 위치 오차 수정

--- Assets/WorkSpace/JYC/Scripts/GameManager.cs
+++ Assets/WorkSpace/JYC/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.SocialPlatforms.Impl;
 using System;
+using UnityEditor;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,14 +20,21 @@ public class GameManager : MonoBehaviour
     [SerializeField] PlayerMove player;
     public float targetY;
     public float recentY;
+    private float errorFix;
+
+    [SerializeField] private float fadeOutFix;
+    [SerializeField] private float startY;
+    [SerializeField] private float endY;
+
 
     [SerializeField] float length;
     public bool isOverY;
     public bool isClearStage;
+    private int cnt;
 
     public int stage;
 
-    private float score;
+    private float score => Mathf.Abs(player.YPos) - Mathf.Abs(fadeOutFix);
     private float screenY => Camera.main.orthographicSize * 2;
 
     void Awake()
@@ -34,10 +42,10 @@ public class GameManager : MonoBehaviour
         if(!instance)
             instance = this;
 
-        
+        fadeOutFix = 0f;
         recentY = player.YPos;
         targetY = recentY - length;
-        stage = 0;
+        stage = cnt = 0;
     }
 
     void Update()
@@ -48,17 +56,19 @@ public class GameManager : MonoBehaviour
         
         if(!isOverY)
         {
+            errorFix = targetY - obstacleManager.lowestY;
             targetY = obstacleManager.lowestY;
+
             isOverY = true;
             return;
         }
             
         if(!isClearStage)
         {
-            
-            score += recentY - targetY;
             isClearStage = true;
             fadeObj.GetComponent<SpriteRenderer>().DOFade(1, fadeTime);
+            startY = player.YPos;
+            
             player.SetDirection(isClearStage);  //플레이어 낙하 방향 조정
             Invoke("FadeOut", waitTime - fadeTime);
             Invoke("Restart", waitTime);
@@ -72,7 +82,9 @@ public class GameManager : MonoBehaviour
 
     void ClearStage()
     {
-        stage ++;
+        if (stage < 2)
+            stage ++;
+        cnt ++;
     }
 
     // 페이드인, 페이드아웃
@@ -84,9 +96,12 @@ public class GameManager : MonoBehaviour
 
     void Restart()
     {
+        endY = player.YPos;
+        fadeOutFix += startY - endY;
         obstacleManager.targetPos = new Vector3(0, player.YPos - screenY / 2, 0);
         recentY = player.YPos;
-        targetY = recentY - length;
+        
+        targetY = recentY - length + errorFix;
 
         isClearStage = false;
         isOverY = false;
@@ -95,7 +110,6 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        score += recentY - player.YPos;
         SaveManager.instance.Save((int)score);
         Time.timeScale = 0f;
     }
