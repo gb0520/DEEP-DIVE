378f6f88177796bb627995f69c949bbc0e6f2def
Atara02 <yeojp73@gmail.com>
2023-12-31 08:52:12 +0900

    별 수정

--- Assets/WorkSpace/YSB/Scripts/GameObj/ItemBase.cs
+++ Assets/WorkSpace/YSB/Scripts/GameObj/ItemBase.cs
@@ -6,7 +6,17 @@ public class ItemBase : ObjBase
 {
     [SerializeField]
     protected int heal;
-
+    protected Vector3 initScale;
+    protected void Awake()
+    {
+        initScale = transform.localScale;
+        transform.localScale = initScale;
+    }
+    private void OnEnable()
+    {
+        transform.localScale = initScale;
+        GetComponent<Collider2D>().enabled = true;
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
