1d6b8ae01267cc5c0ea2e6fa1da64e40e53d38fb
Atara02 <yeojp73@gmail.com>
2023-12-31 02:37:45 +0900

    시간 아이템수정

--- Assets/WorkSpace/YSB/Scripts/GameObj/ItemBase.cs
+++ Assets/WorkSpace/YSB/Scripts/GameObj/ItemBase.cs
@@ -7,6 +7,14 @@ public class ItemBase : ObjBase
     [SerializeField]
     protected int heal;
 
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            target = col.gameObject.GetComponent<PlayerMove>();
+            CrashObj();
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
